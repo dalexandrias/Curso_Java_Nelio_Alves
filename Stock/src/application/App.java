2ff4b37087be37d6ea17200385255221a207fe78
A0129378 <A0129378@NSPECOPE078G6V.REDECORP.BR>
2021-07-16 15:44:28 -0300

    Criado alterado variaveis e inserido atributos na class Product

--- Stock/src/application/App.java
+++ Stock/src/application/App.java
@@ -11,22 +11,22 @@ public class App {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		Product product = new Product();
-		
 		System.out.println("Enter product data: ");
 		System.out.println("Name: ");
-		product.name = sc.nextLine();
+		String name = sc.nextLine();
 		System.out.println("Price: ");
-		product.price = sc.nextDouble();
+		double price = sc.nextDouble();
 		System.out.println("Quantity in stock: ");
-		product.quantity = sc.nextInt();
+		int quantity = sc.nextInt();
+		
+		Product product = new Product(name, price, quantity);
 		
 		System.out.println("Product data: " + product.toString());
 		System.out.println(
 				"Enter the number of products to be added in stock: "
 				);
 		
-		int quantity = sc.nextInt();
+		quantity = sc.nextInt();
 		product.addProducts(quantity);
 		
 		System.out.println("Updated data: " + product.toString());
