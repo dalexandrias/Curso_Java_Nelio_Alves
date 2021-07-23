5a91b1f20cf330d98ab272e39489754e6ca46187
Davi Alexandrias de Oliveira <dalexandrias@hotmail.com>
2021-07-23 00:03:14 -0300

    Utilizando o laco for each

---
+++ LaçoForEach/src/application/App.java
@@ -0,0 +1,11 @@
+package application;
+
+public class App {
+    public static void main(String[] args) {
+        String[] vetor = new String[]{"Davi", "Carla", "Elza", "Edino", "Sara"};
+
+        for (String value : vetor) {
+            System.out.println(value);
+        }
+    }
+}
